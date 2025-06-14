ae6239c51c1ffdf1102d5b586f4316a9fbe34fb7
Deepak <thegreatdeepakpoudel@gmail.com>
2025-06-14 10:18:08 +0545

    Hello world again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-4xl font-bold">Hello World</h1>
+        <h1 className="text-4xl font-bold">Hello World 2</h1>
       </div>
     </main>
   );
